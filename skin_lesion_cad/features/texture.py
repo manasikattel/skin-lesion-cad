295c2d43414a0b1c9dda876e5752105da5164f32
manasikattel <manasikattel1@gmail.com>
2022-10-31 12:47:51 +0100

    Add LBP to texture features script, and exploration notebook

--- skin_lesion_cad/features/texture.py
+++ skin_lesion_cad/features/texture.py
@@ -1,6 +1,6 @@
 # %%
 import random
-from skimage.feature import graycomatrix, graycoprops
+from skimage.feature import graycomatrix, graycoprops, local_binary_pattern
 import numpy as np
 from pathlib import Path
 import cv2
@@ -49,7 +49,36 @@ def get_glcm(image_paths):
             columns={i: f"{col}_{i}" for i in range(8)}, inplace=True)
         all_feat.append(features_dis)
     feat_final_df = pd.concat(all_feat, axis=1)
-    return feat_final_df
+    return feat_final_df.rename(columns={"image_0": "image"})
+
+
+def lbph(image, n_points_radius=[(24, 8), (8, 3), (12, 3), (8, 2), (8, 1)], method="default", eps=1e-7):
+    hist_concat = np.array([])
+    for (n_points, radius) in n_points_radius:
+
+        lbp = local_binary_pattern(
+            image, n_points, radius, method)
+        (hist, _) = np.histogram(lbp.ravel(),
+                                 bins=np.arange(0, n_points + 3),
+                                 range=(0, n_points + 2))
+        # normalize the histogram
+        hist = hist.astype("float")
+        hist /= (hist.sum() + eps)
+        hist_concat = np.append(hist_concat, hist)
+    return hist_concat
+
+
+def get_lbp(image_paths):
+    lbp_feats = []
+    for image_path in tqdm(image_paths):
+        im = cv2.imread(str(image_path))
+        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        lbp_feats.append(lbph(im_gray))
+
+    lbp_df = pd.DataFrame(lbp_feats, columns=[
+        "lbp"+str(i) for i in range(len(lbp_feats[0]))])
+    lbp_df["image"] = [str(i) for i in image_paths]
+    return lbp_df
 
 
 if __name__ == "__main__":
