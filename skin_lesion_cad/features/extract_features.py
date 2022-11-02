e990df45d39ca189362893d8ace7ddd6f74bf1fc
manasikattel <manasikattel1@gmail.com>
2022-11-02 23:30:41 +0100

    Add shape features, optional mask to the texture features

--- skin_lesion_cad/features/extract_features.py
+++ skin_lesion_cad/features/extract_features.py
@@ -1,7 +1,7 @@
 from imageio import save
 import pandas as pd
 from xgboost import train
-from skin_lesion_cad.features import get_glcm, get_lbp, ColorFeaturesExtractor
+from skin_lesion_cad.features import get_glcm, get_lbp, ColorFeaturesExtractor, shape_features
 import cv2
 import multiprocessing as mp
 from tqdm import tqdm
@@ -10,7 +10,20 @@ from pathlib import Path
 import functools as ft
 import random
 
-CHALLENGE = "chall2"
+CHALLENGE = "chall1"
+
+
+def shape_feature(img_path):
+    mask = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
+    img_name = str(img_path).replace("mask", "inpaint")
+    imf_features = shape_features(mask)
+    imf_features['image'] = img_name
+    return imf_features
+
+
+def get_shape(image_paths):
+    res = [shape_feature(image) for image in tqdm(image_paths)]
+    return pd.DataFrame(res)
 
 
 def process_img(img_path, mask_type, cfe):
@@ -60,33 +73,47 @@ def extract_features(image_paths, save_path, mode="train",):
 
     print("Extracting GLCM features")
     glcm_df = get_glcm(image_paths).reset_index(drop=True)
+    # glcm_df_masked = get_glcm(image_paths, masked=True).reset_index(drop=True)
 
     print("Extracting LBP features")
     lbp_df = get_lbp(image_paths).reset_index(drop=True)
+    # lbp_df_masked = get_lbp(image_paths, masked=True).reset_index(drop=True)
 
-    print("Extracting raw color features")
-    color_df_raw = get_color(
-        image_paths, mask_type="ground_truth", color_spaces=color_spaces).reset_index(drop=True)
+    # print("Extracting raw color features")
+    # color_df_raw = get_color(
+    #     image_paths, mask_type="ground_truth", color_spaces=color_spaces).reset_index(drop=True)
 
-    print("Extracting meanshift color features")
-    color_df_ms = get_color(
-        image_paths, mask_type="ground_truth", color_spaces=color_spaces, mshift_params=mshift_params).reset_index(drop=True)
+    # print("Extracting meanshift color features")
+    # color_df_ms = get_color(
+    #     image_paths, mask_type="ground_truth", color_spaces=color_spaces, mshift_params=mshift_params).reset_index(drop=True)
+    # mask_paths = [Path(str(image_path.parent).replace(
+    #     "raw", "processed")) / Path(image_path.name.replace("inpaint", "mask")) for image_path in image_paths]
+
+    # shape_df = get_shape(mask_paths)
+    # shape_df.to_feather(
+    #     save_path/Path(f"{CHALLENGE}_{mode}_shape.feather"))
 
     glcm_df.to_feather(
-        save_path/Path(f"{CHALLENGE}_{mode}_glcm.feather"))
-    lbp_df.to_feather(
-        save_path/Path(f"{CHALLENGE}_{mode}_lbp.feather"))
-    color_df_raw.to_feather(
-        save_path/Path(f"{CHALLENGE}_{mode}_color_raw.feather"))
-    color_df_ms.to_feather(
-        save_path/Path(f"{CHALLENGE}_{mode}_color_meanshift.feather"))
+        save_path/Path(f"{CHALLENGE}_{mode}_glcm_original_image.feather"))
+    # glcm_df_masked.to_feather(
+    #     save_path/Path(f"{CHALLENGE}_{mode}_glcm_masked.feather"))
 
-    dfs = [glcm_df, lbp_df, color_df_raw]
-    all_feat = ft.reduce(
-        lambda left, right: pd.merge(left, right, on='image'), dfs)
-    all_feat.to_feather(
-        save_path/Path(f"{CHALLENGE}_{mode}_all_feat.feather"))
-    return all_feat
+    lbp_df.to_feather(
+        save_path/Path(f"{CHALLENGE}_{mode}_lbp_original_image.feather"))
+    # lbp_df_masked.to_feather(
+    #     save_path/Path(f"{CHALLENGE}_{mode}_lbp_masked.feather"))
+
+    # color_df_raw.to_feather(
+    #     save_path/Path(f"{CHALLENGE}_{mode}_color_raw.feather"))
+    # color_df_ms.to_feather(
+    #     save_path/Path(f"{CHALLENGE}_{mode}_color_meanshift.feather"))
+
+    # dfs = [glcm_df, lbp_df, color_df_raw]
+    # all_feat = ft.reduce(
+    #     lambda left, right: pd.merge(left, right, on='image'), dfs)
+    # all_feat.to_feather(
+    #     save_path/Path(f"{CHALLENGE}_{mode}_all_feat.feather"))
+    return
 
 
 if __name__ == "__main__":
@@ -94,16 +121,16 @@ if __name__ == "__main__":
                     'lab': cv2.COLOR_RGB2LAB,  'YCrCb': cv2.COLOR_RGB2YCrCb}
     mshift_params = {'sp': 10, 'sr': 15}  # spatial and color range radius
 
-    train_path = Path(f"data/processed/{CHALLENGE}/train")
-    val_path = Path(f"data/processed/{CHALLENGE}/val")
+    train_path = Path(f"data/raw/{CHALLENGE}/train")
+    val_path = Path(f"data/raw/{CHALLENGE}/val")
 
     save_path = Path("data/processed/features")
     save_path.mkdir(exist_ok=True, parents=True)
 
-    training_names = train_path.rglob("*inpaint_0*")
+    training_names = train_path.rglob("*.jpg")
     image_paths_training = [i for i in training_names]
 
-    val_names = val_path.rglob("*inpaint_0*")
+    val_names = val_path.rglob("*.jpg")
     image_paths_val = [i for i in val_names]
 
     extract_features(
--- skin_lesion_cad/features/texture.py
+++ skin_lesion_cad/features/texture.py
@@ -8,9 +8,17 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def glcm_features(image, features=["contrast", "dissimilarity", "homogeneity", "energy", "correlation", "ASM"]):
-    m_glcm = graycomatrix(image, distances=[2, 5, 7, 10, 15], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4],
-                          symmetric=True, normed=True)
+def glcm_features(image, features=["contrast", "dissimilarity", "homogeneity", "energy", "correlation", "ASM"], mask=None):
+    if mask is None:
+        m_glcm = graycomatrix(image, distances=[2, 5, 7, 10, 15], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4],
+                              symmetric=True, normed=True)
+    else:
+        masked_image = image.copy()
+        masked_image[masked_image == 0] = 1
+        masked_image[~mask] = 0
+
+        m_glcm = graycomatrix(masked_image, distances=[2, 5, 7, 10, 15], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4],
+                              symmetric=True, normed=True)[1:, 1:, :, :]
     feature_dict = {}
     for feature in features:
         feature_dict[feature] = [np.concatenate(
@@ -29,13 +37,22 @@ def get_chall2_class(path):
         raise ValueError("class needs to be bcc, mel or scc")
 
 
-def get_glcm(image_paths):
+def get_glcm(image_paths, masked=False):
     feature_dfs = []
     for image_path in tqdm(image_paths):
         img = cv2.imread(str(image_path))
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if masked:
+            mask_path = Path(str(image_path.parent).replace(
+                "raw", "processed")) / Path(image_path.name.replace("inpaint", "mask"))
+            mask_img = cv2.imread(str(mask_path))
+            mask_img = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
+            mask_img = mask_img > 127
 
-        glcm_features_dict = glcm_features(gray_img)
+            glcm_features_dict = glcm_features(gray_img, mask=mask_img)
+        else:
+            glcm_features_dict = glcm_features(gray_img)
+
         feature_df = pd.DataFrame(glcm_features_dict)
         feature_df["image"] = str(image_path)
         feature_dfs.append(feature_df)
@@ -51,15 +68,21 @@ def get_glcm(image_paths):
     return feat_final_df.rename(columns={"image_0": "image"})
 
 
-def lbph(image, n_points_radius=[(32, 3), (24, 3), (16, 2), (12, 5), (12, 3), (12, 2), (8, 3), (8, 2), (8, 1)], method="uniform", eps=1e-7):
+def lbph(image, mask=None, n_points_radius=[(32, 3), (24, 3), (16, 2), (12, 5), (12, 3), (12, 2), (8, 3), (8, 2), (8, 1)], method="uniform", eps=1e-7):
     hist_concat = np.array([])
     for (n_points, radius) in n_points_radius:
 
         lbp = local_binary_pattern(
             image, n_points, radius, method)
-        (hist, _) = np.histogram(lbp.ravel(),
-                                 bins=np.arange(0, n_points + 3),
-                                 range=(0, n_points + 2))
+        if mask is not None:
+            (hist, _) = np.histogram(lbp.ravel(),
+                                     bins=np.arange(0, n_points + 3),
+                                     range=(0, n_points + 2))
+        else:
+            (hist, _) = np.histogram(lbp[mask],
+                                     bins=np.arange(0, n_points + 3),
+                                     range=(0, n_points + 2))
+
         # normalize the histogram
         hist = hist.astype("float")
         hist /= (hist.sum() + eps)
@@ -67,13 +90,22 @@ def lbph(image, n_points_radius=[(32, 3), (24, 3), (16, 2), (12, 5), (12, 3), (1
     return hist_concat
 
 
-def get_lbp(image_paths):
+def get_lbp(image_paths, masked=False):
 
     lbp_feats = []
     for image_path in tqdm(image_paths):
         im = cv2.imread(str(image_path))
         im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        lbp_feats.append(lbph(im_gray))
+        if masked:
+            mask_path = Path(str(image_path.parent).replace(
+                "raw", "processed")) / Path(image_path.name.replace("inpaint", "mask"))
+            mask_img = cv2.imread(str(mask_path))
+            mask_img = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
+            mask_img = mask_img > 127
+
+            lbp_feats.append(lbph(im_gray, mask=mask_img))
+        else:
+            lbp_feats.append(lbph(im_gray))
 
     lbp_df = pd.DataFrame(lbp_feats, columns=[
         "lbp"+str(i) for i in range(len(lbp_feats[0]))])
@@ -89,20 +121,19 @@ if __name__ == "__main__":
     train_path = Path(f"data/processed/{chall}/train")
 
     training_names = train_path.rglob("*/*inpaint_0*")
-    image_paths = [i for i in training_names]
+    image_paths = [i for i in training_names][:10]
     image_classes = [0 if ("nevus" in str(i)) else 1 for i in image_paths]
     # mask_paths = [Path(str(image_path.parent).replace("raw", "processed")) /
     #               Path(image_path.stem+"_mask_1_0.png") for image_path in image_paths]
 
     print("Extracting GLCM features")
 
-    feat_final_df = get_glcm(image_paths)
-
+    feat_final_df = get_glcm(image_paths, masked=True)
     if chall == "chall1":
         feat_final_df["class"] = feat_final_df["image_0"].apply(
             lambda x: "nevus" if "nevus" in x else "others")
     else:
-        feat_final_df["class"] = feat_final_df["image_0"].apply(
+        feat_final_df["class"] = feat_final_df["image"].apply(
             get_chall2_class)
     feat_final_df.reset_index(inplace=True)
     feat_final_df.to_feather(output_dir/Path(f"glcm_features_{chall}.feather"))
