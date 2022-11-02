7fc860fe0268f21c82f85040497050c64ff2176a
manasikattel <manasikattel1@gmail.com>
2022-11-02 23:31:20 +0100

    Update init

--- skin_lesion_cad/features/__init__.py
+++ skin_lesion_cad/features/__init__.py
@@ -1,3 +1,4 @@
 from .texture import get_glcm, get_lbp
 from .colour import ColorFeaturesExtractor
-__all__ = [get_glcm, get_lbp, ColorFeaturesExtractor]
+from .shape import shape_features
+__all__ = [get_glcm, get_lbp, ColorFeaturesExtractor, shape_features]
