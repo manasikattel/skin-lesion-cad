fix mwnl loss computation

skin_lesion_cad/training/models/swin.py
@@ -36,14 +36,15 @@ class SwinModel(LightningModule):
                  learning_rate=1e-4,
                  weights="IMAGENET1K_V1",
                  loss: str = "cross_entropy",
-                 chkp_pretrained: str|None=None):
+                 chkp_pretrained: str|None=None,
+                 device=None):
         
         super().__init__()
 
         # save the hyperparameters
         self.learning_rate = learning_rate
         self.loss = loss
-        self.criterion = get_loss(loss)
+        self.criterion = get_loss(loss, device=device, num_classes=num_classes)
         self.num_classes = num_classes
         self.chkp_pretrained = chkp_pretrained
         
@@ -134,6 +135,11 @@ class SwinModel(LightningModule):
                     on_step=True,  on_epoch=True,
                     prog_bar=True, logger=True,
                     batch_size=batch_size)
+    def training_epoch_end(self, training_step_outputs):
+        # do something with all training_step outputs
+        # for out in training_step_outputs:
+        if self.loss == 'mwn':
+            self.criterion.reset_epoch(self.current_epoch)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)

skin_lesion_cad/utils/training_utils.py
@@ -2,8 +2,12 @@ import torch
 import torch.nn as nn
 import kornia.losses as losses
 from skin_lesion_cad.training.losses import MWNLoss
+from skin_lesion_cad.config.mwn_config import _C as cfg
 
-def get_loss(name):
+mwnl_params = {'gamma':0.6, 'beta':0.1, 'type':'fix', 'sigmoid':'normal',
+               'num_class_list':None, 'cfg':cfg, 'device':None}
+
+def get_loss(name, device=None, num_classes=None):
     if name == 'cross_entropy':
         return  nn.CrossEntropyLoss()
     elif name == 'focal':
@@ -11,7 +15,11 @@ def get_loss(name):
     elif name == 'tversky':
         return losses.TverskyLoss(0.4, 0.4)
     elif name == 'mwnl':
-        return MWNLoss()
+        mwnl_params['device'] = f'cuda:{device}'
+        mwnl_params['num_class_list'] = list(range(num_classes))
+        loss = MWNLoss(para_dict=mwnl_params)
+        loss.reset_epoch(0)
+        return loss
     
     else:
         raise ValueError(f'Loss {name} not supported')